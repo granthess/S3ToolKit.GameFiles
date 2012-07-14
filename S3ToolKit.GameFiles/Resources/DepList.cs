3d6355860e4421a42ba8918ccf8c4f8e44b0da17
Grant Hess <granthes@the-prof.net>
2012-07-14 00:52:03 -0600

    Fixing License Header
    Replacing Foobar in license header with CC Magic.

--- S3ToolKit.GameFiles/Resources/DepList.cs
+++ S3ToolKit.GameFiles/Resources/DepList.cs
@@ -14,7 +14,7 @@
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with CC Magic.  If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
 using System.Collections.Generic;
