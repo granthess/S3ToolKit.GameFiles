a87a09579ab3ac9374a5b83aac4c158171725ca5
Grant Hess <granthes@the-prof.net>
2012-08-06 14:57:06 -0600

    Adding LGPL header to the files in Library

--- S3ToolKit.GameFiles/Library/ResourceList.cs
+++ S3ToolKit.GameFiles/Library/ResourceList.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+    Copyright 2012, Grant Hess
+
+    This file is part of S3ToolKit.GameFiles.
+
+    S3ToolKit.Utils is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with CC Magic.  If not, see <http://www.gnu.org/licenses/>.
+*/
+yusing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
