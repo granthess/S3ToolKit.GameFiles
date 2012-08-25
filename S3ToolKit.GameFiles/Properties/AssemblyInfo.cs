9c9993bb8a0c8cc23c5c43b139c577b63201d555
Grant Hess <granthes@the-prof.net>
2012-08-24 19:01:20 -0600

    Adding NuGet support
    Added NuGet publish support

--- S3ToolKit.GameFiles/Properties/AssemblyInfo.cs
+++ S3ToolKit.GameFiles/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("S3ToolKit.GameFiles")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library to encapsulate the reading and writing of the game files used by The Sims 3 from Electronic Arts.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("the-prof.net")]
 [assembly: AssemblyProduct("S3ToolKit.GameFiles")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+
